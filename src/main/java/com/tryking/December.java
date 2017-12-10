2e8f6973cf81b8ac09e2eccdbe0a2ac1b5a77613
dengkaiting <dengkaiting@migu.cn>
2017-12-10 23:19:39 +0800

    Update second today

--- src/main/java/com/tryking/December.java
+++ src/main/java/com/tryking/December.java
@@ -13,6 +13,7 @@ public class December {
 
     /**
      * 561. Array Partition I
+     * <p>
      * Given an array of 2n integers, your task is to group these integers into n pairs of integer,
      * say (a1, b1), (a2, b2), ...,(an, bn)
      * which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
@@ -73,5 +74,40 @@ public class December {
             res += nums[i];
         }
         return res;
+    }
+
+    /**
+     * 461.Hamming Distance
+     * <p>
+     * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+     * Given two integers x and y, calculate the Hamming distance.
+     * 没有思路
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public int hammingDistance(int x, int y) {
+        return 0;
+    }
+
+    /**
+     * 标准答案：对进制转换还是不熟
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public int hammingDistance0(int x, int y) {
+        int Distance = 0;
+
+        while (x != 0 || y != 0) {
+            if (x % 2 != y % 2) {
+                Distance++;
+            }
+            x /= 2;
+            y /= 2;
+        }
+        return Distance;
     }
 }
